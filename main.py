f54aa588d395b5e458baae1b80255a24ebae0d58
BostonSlettum22 <816661@mystma.org>
2020-12-17 19:09:55 +0000

    topfivemovies 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def topFiveMovies(a, b, c, d, e):
-    return a, b, c, d, e
+    return a + " " + b + " " + c + " " + d + " " + e 
 
 print (topFiveMovies("Fast Times at Ridge Mount High", "Spiderman Far From Home","Iron man 3", "First Man", "Breakfest Club"))
 
